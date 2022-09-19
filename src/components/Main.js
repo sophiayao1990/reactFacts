ae40cb6bd7e8870708a788335688fc8b432b4cf5
Jingfei Yao <sophiayao1990@gmail.com>
2022-09-19 15:00:56 -0400

    main and master branch

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,6 +8,7 @@ function Main() {
                 <li>Has well over 100K stars on GitHub</li>
                 <li>Is maintained by Facebook</li>
                 <li>Powers thousands of enterprise apps, including mobile apps</li>
+                <li>test</li>
             </ul>
         </div>        
     )
